bbd37e570ce2f00cd253400ac53dee85f3789e33
HyunJi <138995038+gyummo@users.noreply.github.com>
2024-03-14 13:11:52 +0900

    [Bronze V] Title: 구구단, Time: 236 ms, Memory: 18616 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		for(int n = 2; n <= 2; n++){
+			for(int j = 1; j <= 9; j++){
+				System.out.println(num + " * " + j + " = " + (num * j));
+			}
+
+			System.out.println();
+
+		}
+	}
+}
